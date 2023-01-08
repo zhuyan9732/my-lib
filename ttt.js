9b66249ffd93d82edb48e35023660f5ad69ca381
颜梓扬 <yanziyang@flyele.net>
2023-01-09 01:08:22 +0800

    delete noless file

--- ttt.js
+++
@@ -1,9 +0,0 @@
-console.log(window.React);
-
-import "./lib/my-lib.iife";
-
-console.log(YUI);
-
-const React = window.React;
-
-// React.render(React.createElement(), document.querySelector("#root"));
